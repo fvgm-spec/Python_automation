b05c0ae0157886705596f58c11a185a1861cfaa0
fvgm-spec <felixvidalgu@gmail.com>
2020-09-07 00:09:34 -0300

    adding functions

---
+++ dictionaries/count_letters.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def count_letters(text):
+	result = {}
+	for letter in text:
+		if letter not in result:
+			result[letter] = 0
+		result[letter] += 1
+	return result
+	
+print(count_letters("tenant"))
---
+++ dictionaries/email_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+ef email_list(domains):
+	emails = []
+	for domain, users in domains.items():
+	  for user in users:
+	    emails.append(user + '@' + domain)
+	return(emails)
+
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
